ec7521a65187128c9d787fa8ec6314234f8f7500
kazuya1886 <kazuyasegawa0507@gmail.com>
2025-10-04 18:16:46 +0900

    Add files via upload

--- kadai_010/summary.js
+++ kadai_010/summary.js
@@ -3,20 +3,22 @@ $(function(){
     'click',function(){
       $("#target").css('color',"red");
   });
-
+});
+$(function(){ 
   $('#change-text').on(
     'click',function(){
       $('#target').text('HELLO!');
-   
+   });
+  });
+$(function(){   
   $('#fade-out').on(
     'click',function(){
     $('#target').fadeOut();
-  
+   });
+  });
+$(function(){
   $('#fade-in').on(
     'click',function(){
       $('#target').fadeIn();
-    }
-  )  
-    });   
-  });
+  }); 
 });
